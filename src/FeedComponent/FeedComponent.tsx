f79be9de68db72c97b9bf3ae31ef41dcb1459609
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2023-01-26 21:11:55 +0100

    feat: add pagination

--- src/FeedComponent/FeedComponent.tsx
+++ src/FeedComponent/FeedComponent.tsx
@@ -1,20 +1,47 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useRef} from 'react';
 import {getRandomFeedDataObservable} from '../faker';
-import {map} from 'rxjs';
+import {catchError, defer, EMPTY, exhaustMap, filter, from, map, mergeAll, scan, startWith, switchMap, tap} from 'rxjs';
 import {useObservedValue} from '../hooks/observable';
 import {FeedItem} from '../models';
 import classes from './FeedComponent.module.css';
+import {fromMutationObserver} from '../fromMutationObserver';
+import {fromIntersectionObserver} from '../fromIntersectionObserver';
 
 function FeedComponent() {
-  const feed$ = useMemo(() => getRandomFeedDataObservable().pipe(
-    map(({ items }) => items)
-  ), []);
+  const nextPageRef = useRef<number | null>(1);
+  const articlesDiv = useRef(null);
+
+  const loadMore$ = useMemo(() => defer(() => fromMutationObserver(articlesDiv.current!, {
+    childList: true,
+    subtree: true
+  }).pipe(
+    map(([record]) => (record.target as HTMLElement).tagName === 'DIV'
+      ? Array.from(record.target.childNodes)
+      : record.target.parentElement?.tagName === 'DIV'
+        ? Array.from(record.target.parentElement.childNodes)
+        : []), // select all <article> tags
+    map(elements => elements.splice(-3)), // take the last 3 <article> tags
+    switchMap(elements => from(elements.map(el => fromIntersectionObserver(el as Element))).pipe(mergeAll())),
+    filter(({isIntersecting}) => isIntersecting),
+    startWith(null)
+  )), []);
+
+  const feed$ = useMemo(() => loadMore$.pipe(
+    exhaustMap(() => nextPageRef.current
+      ? getRandomFeedDataObservable({nextPage: nextPageRef.current}).pipe(
+        catchError(() => EMPTY),
+        tap({
+          next: ({nextPage}) => nextPageRef.current = nextPage
+        })
+      )
+      : EMPTY),
+    scan((acc, {items}) => acc.concat(items), [] as FeedItem[])), [loadMore$]);
 
   const feedItems = useObservedValue<FeedItem[]>(feed$, []);
 
   return (
     <>
-      <div className={classes.articles}>
+      <div className={classes.articles} ref={articlesDiv}>
         {feedItems.map((item) => (
           <article id={item.id} key={item.id}>
             <div className={classes.user}>
